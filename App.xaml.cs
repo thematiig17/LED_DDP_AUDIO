43f975400bfa991f82fa724d4b90bae30bbdf808
HATSUNE <64473107+thematiig17@users.noreply.github.com>
2026-07-05 12:24:57 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LED_DDP_DRIVER
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
